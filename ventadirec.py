1749eed1f270640b77c3ebda73c766b835b5b6a1
JeanJaramillo <81722915+JeanJaramillo@users.noreply.github.com>
2021-11-26 23:21:49 -0500

    ventadirec.py ok

--- ventadirec.py
+++ ventadirec.py
@@ -1,27 +1,45 @@
 #trabajando...
 import pymongo
+import os
 
 client = pymongo.MongoClient("mongodb+srv://jean-rafael:lokazo.420@clustercertus.6mvum.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+unaDireccion ={}
+
 db = client.db_gcp
 
 coll = db.ventadirec
 
-coll.insert_many([
-    {'nombre':'Lokazo thc',
-    'apellidos':'Cuatro Veinte',
-    'provincia':'Lima',
-    'distrito':'Breña',
-    'calle':'Jr. Marhihuana',
-    'numero':'420',
-    'interior':'666',
-    'telefonos':['992453123', '929351202']},
-    {'nombre':'Anacleto Cocaina',
-    'apellidos':'Sopota Madre',
-    'provincia':'Tacna',
-    'distrito':'Miraflores',
-    'calle':'Jr. LSD',
-    'numero':'69',
-    'interior':'420',
-    'telefonos':['999555333', '988977344']}
-])
+os.system('cls')
+print('INFORMACIÓN DE ENTREGA')
+nombre = input('Nombres: ')
+apellidos = input('Apellidos: ')
+provincia = input('Provincia: ')
+distrito = input('Distrito: ')
+calle = input('Calle: ')
+numero = input('Numero: ')
+interior = input('Interior: ')
+telefonos= input('Telefonos - Si son más de uno separalos por comas (,): ')
+
+misTelefonos = []
+for telefono in telefonos.split(','):
+    misTelefonos.append(telefonos)
+
+unaDireccion['nombre'] = nombre
+unaDireccion['apellidos'] = apellidos
+unaDireccion['provincia'] = provincia
+unaDireccion['distrito'] = distrito
+unaDireccion['calle'] = calle
+unaDireccion['numero'] = numero
+unaDireccion['interior'] = interior
+unaDireccion['telefonos'] = telefonos
+
+print('Grabando su información...')
+dato = coll.insert_one(unaDireccion)
+
+os.system('cls')
+print('Los datos de la venta fueron guardados en el Id: ', dato.inserted_id)
+
+
+
+
 
