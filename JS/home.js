b34eecfdbfe66ba909b6393b5a0c3c022947a387
JeanJaramillo <81722915+JeanJaramillo@users.noreply.github.com>
2021-12-16 17:48:27 -0500

    Eliminanción de palntilla y diseño anterior

--- JS/home.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoades', () => {
-    const marcasCarrucel = document.querySelectorAll('.carrucel');
-    M.Carousel.init(marcasCarrucel, {
-        duration:150
-    })
-})
